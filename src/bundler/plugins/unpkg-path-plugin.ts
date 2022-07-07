a338497eeceed1def804aab1ad66aba8dc9b15ba
Daniel Sastre Villaseñor <daniel.sastre@netlogistik.local>
2022-07-07 17:59:08 -0500

    Added draggable code cells

--- src/bundler/plugins/unpkg-path-plugin.ts
+++ src/bundler/plugins/unpkg-path-plugin.ts
@@ -1,16 +1,4 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localForage from 'localforage';
-
-const fileCache = localForage.createInstance({
-  name: 'filecache'
-});
-
-(async() => {
-  await fileCache.setItem('color', 'red');
-  const color = await fileCache.getItem('color');
-  console.log(color);
-})()
 
 export const unpkgPathPlugin = () => {
   return {
--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,11 @@
-import * as esbuild from 'esbuild-wasm'
-import { useState, useEffect, useRef } from 'react';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDom from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const ref = useRef<any>();
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-  
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    });
-    
-  }
-  
-  useEffect(() => {
-    startService();
-  }, []);
-  
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    })
-    // console.log(result)
-    setCode(result.outputFiles[0].text);
-    try { 
-      eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
-  }
-  
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <pre>{code}</pre>
-      <iframe src="/test.html" sandbox=''></iframe>
+      <CodeCell/>
     </div>
   );
 };
